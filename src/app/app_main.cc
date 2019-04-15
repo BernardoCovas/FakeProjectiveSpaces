01a2d2c3de7db1de77fe3936d75293091dac185d
Bernardo Covas <bernardo.covas@outlook.com>
2019-04-15 07:15:58 +0100

    Tweaked app_main test

--- src/app/app_main.cc
+++ src/app/app_main.cc
@@ -1,6 +1,7 @@
 #include <LibFkps.h>
 
 #include <stdio.h>
+#include <thread>
 #include <string>
 #include <vector>
 #include <fstream>
@@ -8,7 +9,10 @@
 int main(int argc, char* argv[])
 {
 	if (argc < 2)
+	{
 		printf("Args: Parsed_Det_File.txt\n");
+		return 1;
+	}
 
 	std::ifstream infile(argv[1]);
 	std::vector<FKPS> fkpsV;
@@ -19,16 +23,17 @@ int main(int argc, char* argv[])
 	{
 		++currline;
 	
-		FKPS fkps;
+		FKPS fkps = nullptr;
 		LibFkpsErr_t err_code;
 		int libinfoN;
 		int libinfoK;
 		
 		char* express = new char[line.size()];
 		int n = std::sscanf(line.c_str(), "%d,%d,%[^\t\n]", &libinfoN, &libinfoK, express);
-		if (n < 3) { delete[] express; continue; }
-
+		if (n < 3) goto LIBLOAD_FAILED;
+		
 		err_code = LibFkpsInit(&fkps, libinfoN, libinfoK, express);
+		delete[] express;
 		if (err_code != LIBFKPS_ERR_SUCCESS)
 			goto LIBLOAD_FAILED;
 		
@@ -40,7 +45,6 @@ int main(int argc, char* argv[])
 		if (err_code != LIBFKPS_ERR_SUCCESS)
 			goto LIBLOAD_FAILED;
 
-
 		fkpsV.push_back(fkps);
 		printf("Completed: %d with: %s\n", currline, LibFkpsErrToChar(err_code));
 		continue;
@@ -49,13 +53,16 @@ int main(int argc, char* argv[])
 		
 		printf("Failed: %d with code: %s\n", currline, LibFkpsErrToChar(err_code));
 		LibFkpsDeinit(fkps);
-
-		delete[] express;
 	}
 
+	infile.close();
+
+	int i = 0;
 	for (FKPS lib : fkpsV)
 	{
-		// TODO (bcovas)
+
+		LibFkpsDeinit(lib);
+		printf("Unloaded: %d\n", i++);
 	}
 
 
