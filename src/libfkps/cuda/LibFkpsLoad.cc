f9cd339b3cf44aa1d952655ae891f431531ef3da
Bernardo Covas <bernardo.covas@outlook.com>
2019-04-15 07:16:28 +0100

    Added non-cuda Load implementation.

---
+++ src/libfkps/cuda/LibFkpsLoad.cc
@@ -0,0 +1,13 @@
+#include <LibFkps.h>
+
+#include "../LibFkps.hh"
+
+#include <dlfcn.h>
+
+LibFkpsErr_t LibFkpsLoad(FKPS _lib)
+{
+	LibFkps_t* lib = (LibFkps_t*)_lib;
+
+	// TODO (bcovas)
+	return LIBFKPS_ERR_SUCCESS;
+}
